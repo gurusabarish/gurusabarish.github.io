f20ac7d2e00c01f81064a7762e963998653c9fab
Guru Sabarish <gurusabarisha@gmail.com>
2021-09-03 00:22:17 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
-        output: `./sitemap.xml`,
+        output: `/sitemap.xml`,
         exclude: [`/404.html`],
       },
     },
